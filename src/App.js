8bae519cf72201ce9b4bd3cdfa356a231d7fcf41
yuyeong choi <imdud0612@gmail.com>
2020-10-05 19:40:21 +0900

    add global style

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import styled from "styled-components";
+import GlobalStyle from "./GlobalStyles";
 import Movie from "./Movie";
+
+const container = styled.section`
+  
+`;
 
 function App() {
   const [loading, setLoading] = useState(true);
@@ -18,10 +24,26 @@ function App() {
 
   return (
     <div className="App">
-      <div>{loading ? 'Loading ... ' : movies.map(movie => {
-          const { id, year, title, summary, medium_cover_image } = movie;
-          return <Movie key={id} id={id} year={year} title={title} summary={summary} poster={medium_cover_image} />
-      })}</div>
+      <GlobalStyle />
+      <section className="container">
+          {loading
+              ? <div className="loader">
+                  <span className="loader_text">Loading ... </span>
+                </div>
+              : <div className="movies">
+                  {movies.map(movie => (
+                      <Movie key={movie.id}
+                             id={movie.id}
+                             year={movie.year}
+                             title={movie.title}
+                             rating={movie.rating}
+                             summary={movie.summary}
+                             poster={movie.medium_cover_image}
+                             genres={movie.genres} />
+                  ))}
+              </div>
+          }
+      </section>
     </div>
   );
 }
--- src/Movie.js
+++ src/Movie.js
@@ -1,9 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from "styled-components";
 
-function Movie({ id, year, title, summary, poster }) {
-    return (
-        <h5>{title}</h5>
+const movie = styled.article`
+
+`;
+
+function Movie({ id, year, title, summary, poster, genres, rating }) {
+
+    return(
+        <div className="movie">
+            <article>
+                <img src={poster} alt={title} title={title} />
+                <div className="movie_data">
+                    <h3 className="movie_title">{title}</h3>
+                    <h5 className="movie_year">{year}</h5>
+                    <ul className="genres">
+                        {genres && genres.map((genre, index) => <li key={index} className="genres_genre">{genre}</li>)}
+                    </ul>
+                    <strong className="movie_rating">{rating}</strong>
+                    <p className="movie_summary">{summary}</p>
+                </div>
+            </article>
+        </div>
     );
 }
 
@@ -11,8 +30,10 @@ Movie.propTypes = {
     id: PropTypes.number.isRequired,
     year: PropTypes.number.isRequired,
     title: PropTypes.string.isRequired,
+    rating: PropTypes.number.isRequired,
     summary: PropTypes.string.isRequired,
-    poster: PropTypes.string.isRequired
+    poster: PropTypes.string.isRequired,
+    genres: PropTypes.arrayOf(PropTypes.string).isRequired
 }
 
 export default Movie;
