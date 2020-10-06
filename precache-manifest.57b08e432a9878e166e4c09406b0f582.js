b49aee8eba88aaf4b82aae7736696127260017de
yuyeong choi <imdud0612@gmail.com>
2020-10-06 19:26:39 +0900

    Updates

---
+++ precache-manifest.57b08e432a9878e166e4c09406b0f582.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbd9b7ff56096811a2bd376d12eb3c22",
+    "url": "/github.io/movie-app/index.html"
+  },
+  {
+    "revision": "da6e3049d1c1d2616221",
+    "url": "/github.io/movie-app/static/js/2.3c7bf1f8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/github.io/movie-app/static/js/2.3c7bf1f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76a1d8d716b42a745159",
+    "url": "/github.io/movie-app/static/js/main.449ef2a0.chunk.js"
+  },
+  {
+    "revision": "125ca468f7a1bf465638",
+    "url": "/github.io/movie-app/static/js/runtime-main.b63b09b6.js"
+  }
+]);
