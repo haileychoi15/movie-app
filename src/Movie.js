3e601662cd96f08bd0968fc7fa661e6fb3592190
yuyeong choi <imdud0612@gmail.com>
2020-10-06 18:18:31 +0900

    stying upgrade

--- src/Movie.js
+++ src/Movie.js
@@ -8,7 +8,10 @@ const MovieBlock = styled.div`
   justify-content: center;
   width: 100%;
   margin: 0 2% 70px;
+  border-radius: 6px;
   background: #fff;
+  box-shadow: 0 13px 27px -5px rgba(50, 50, 93, 0.5);
+              
   @media only screen and (min-width: 900px) {
     width: 46%;
     margin-bottom: 100px;
@@ -16,27 +19,41 @@ const MovieBlock = styled.div`
 `;
 
 const MovieArticle = styled.article`
-  width: 90%;
+  width: 92%;
   display: flex;
   justify-content: center;
 `;
 
 const MoviePoster = styled.div`
   position: relative;
-  min-width: 120px;
+  min-width: 80px;
   height: auto;
   margin-right: 6%;
+  @media only screen and (min-width: 480px) {
+    min-width: 120px;
+  }
+  @media only screen and (min-width: 768px) {
+    min-width: 150px;
+  }
+  @media only screen and (min-width: 900px) {
+    min-width: 120px;
+  }
+  @media only screen and (min-width: 1200px) {
+    min-width: 150px;
+  }
 `;
 
 const PosterImage = styled.img`
   position: absolute;
   top: -30px;
   width: 100%;
+  height: auto;
+  box-shadow: 0 13px 27px -5px rgba(50, 50, 93, 0.5);
 `;
 
 const MovieData = styled.dl`
   flex-grow: 1;
-  padding: 5% 0;
+  padding: 3% 0;
 `;
 
 const MovieTitle = styled.dt`
@@ -56,7 +73,7 @@ const MovieTitle = styled.dt`
 `;
 
 const MovieYear = styled.dd`
- margin-bottom: 5px;
+  margin-bottom: 5px;
   font-size: 15px;
   font-weight: 300;
 `;
@@ -68,6 +85,7 @@ const MovieGenres = styled.dd`
 `;
 
 const MovieRating = styled.dd`
+  margin-bottom: 10px;
   font-size: 16px;
 `;
 
@@ -78,6 +96,8 @@ const Stars = styled.span`
 `;
 
 const MovieSummary = styled.dd`
+  font-weight: 300;
+  color: gray;
   @media only screen and (min-width: 480px) {
     font-size: 14px;
   }
@@ -132,7 +152,7 @@ function Movie({ id, year, title, summary, poster, genres, rating }) {
                         </strong>
                     </MovieRating>
                     <MovieSummary>
-                        <p>{summary}</p>
+                        <p>{summary.slice(0, 200)}</p>
                     </MovieSummary>
                 </MovieData>
             </MovieArticle>
